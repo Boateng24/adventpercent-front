dcda8f0c92435442e66adfb21e30123d42345155
tuffourboateng-amalitech <99021465+tuffourboateng-amalitech@users.noreply.github.com>
2024-01-31 08:20:44 +0000

    auth base url updated

--- src/api/backend.api.js
+++ src/api/backend.api.js
@@ -1,4 +1,4 @@
-export const authBase = "https://adventpercent.onrender.com/auth/";
+export const authBase = "https://adventpercent.onrender.com/auth";
 export const songBase = "https://adventpercent.onrender.com/songs";
 
 // export const authBase = "http://localhost:5000/auth";
