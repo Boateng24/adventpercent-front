b3c94f4c7fcccdf8018607f943006a84f3a8577a
tuffourboateng-amalitech <99021465+tuffourboateng-amalitech@users.noreply.github.com>
2024-01-31 08:16:10 +0000

    hosted url updated in the app

--- src/api/backend.api.js
+++ src/api/backend.api.js
@@ -1,2 +1,5 @@
-export const authBase = "http://localhost:5000/auth";
-export const songBase = "http://localhost:5000/songs";
+export const authBase = "https://adventpercent.onrender.com/auth/";
+export const songBase = "https://adventpercent.onrender.com/songs";
+
+// export const authBase = "http://localhost:5000/auth";
+// export const songBase = "http://localhost:5000/songs";
