21cd56fbaddca3040a11e184dd3eaacab38b7b92
Boateng24 <tuffourboateng2@gmail.com>
2025-07-04 11:07:25 +0000

    render api's enabled

--- src/api/backend.api.js
+++ src/api/backend.api.js
@@ -1,7 +1,7 @@
-// export const authBase = "https://adventpercent.onrender.com/auth";
-// export const songBase = "https://adventpercent.onrender.com/songs";
-// export const uploadBase = "https://adventpercent.onrender.com/uploadsongs";
+export const authBase = "https://adventpercent.onrender.com/auth";
+export const songBase = "https://adventpercent.onrender.com/songs";
+export const uploadBase = "https://adventpercent.onrender.com/uploadsongs";
 
-export const authBase = "http://localhost:5000/auth";
-export const songBase = "http://localhost:5000/songs";
-export const uploadBase = "http://localhost:5000/uploadsongs"
+// export const authBase = "http://localhost:5000/auth";
+// export const songBase = "http://localhost:5000/songs";
+// export const uploadBase = "http://localhost:5000/uploadsongs"
